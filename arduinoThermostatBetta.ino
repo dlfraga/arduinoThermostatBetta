5ded88d9a384c9092f8505db7d46236e917cca01
Dlfraga <dlfraga@users.noreply.github.com>
2016-05-02 23:32:46 -0300

    Logic improved

--- arduinoThermostatBetta.ino
+++ arduinoThermostatBetta.ino
@@ -8,13 +8,16 @@ const int ONE_WIRE_BUS = 7;
 //relay pin on the board
 const int RELAY_PIN = 6;
 //update interval for main logic
-const long UPDATE_INTERVAL = 2000;
+const long UPDATE_INTERVAL = 500;
 //used to mantain the state machine
 long previousMillis = 0;
 //target temperature.
 const float targetTemp = 26;
 //temperature tolerance. The program will try to maintain the temperature beetween the targetTemp - tolerance $$
-const float TEMP_TOLERANCE = 1;
+const float TEMP_TOLERANCE = 0.5;
+//lm25 sensor offset. it's used to increase 
+//the read temp a bit because the sensor isn't actually underwater
+const float SENSOR_OFFSET = 1;
 // Onewire bus
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -28,37 +31,46 @@ float tempWater = 0;
 bool tempAchieved = true;
 //controls the relay state
 int rlstate = LOW;
+//controls how much time to wait before activating the relay
+const long RELAY_GRACETIME = 10000;
+
 void loop()
 {
   //To avoid using delay() we use a state machine
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis > UPDATE_INTERVAL) {
-    previousMillis = currentMillis;    
+    previousMillis = currentMillis;
     sensors.requestTemperatures();
     tempAmbient = sensors.getTempC(sensor1);
-    tempWater = read_lm35_temperature(LM_35_APIN);
-    
-   //delay reactivating the relay until we are completely below the target temp
-   //this is done to avoid excessive flapping of the relay
+    tempWater = readLm35Temp(LM_35_APIN);
+
+    //delay reactivating the relay until we are completely below the target temp
+    //this is done to avoid excessive flapping of the relay
     if (tempAchieved) {
       if (tempWater < (targetTemp - TEMP_TOLERANCE)) {
         tempAchieved = false;
       }
     } else {
       if (tempWater < (targetTemp - TEMP_TOLERANCE) || tempWater < (targetTemp + TEMP_TOLERANCE) ) {
-        rlstate = HIGH;                
+        rlstate = HIGH;        
       } else {
         rlstate = LOW;
         tempAchieved = true;
       }
     }
-    digitalWrite(RELAY_PIN, rlstate);
+    //wait RELAY_GRACETIME time before activating the relay
+    //this is done to allow all components do stabilize
+    if (currentMillis > RELAY_GRACETIME) {      
+      digitalWrite(RELAY_PIN, rlstate);
+      //redraw the LCD in case of errors.
+      printDefaultLCDText();
+    }    
   }
   //update the lcd with the current temperatures
   lcd.setCursor(5, 0);
   lcd.print(tempAmbient);
   lcd.setCursor(5, 1);
-  lcd.print(tempWater);  
+  lcd.print(tempWater);
 }
 
 void setup()
@@ -69,7 +81,7 @@ void setup()
   printDefaultLCDText();
 }
 
-float read_lm35_temperature(int lmanalogpin)
+float readLm35Temp(int lmanalogpin)
 {
   double referenceVoltage = 1.1; //Set to 5, 3.3, 2.56 or 1.1 depending on analogReference Setting
   float tempInC = 0;
@@ -77,7 +89,7 @@ float read_lm35_temperature(int lmanalogpin)
   for (int i = 0; i < 10; i++) { // Average 10 readings to increase accuracy
     reading += analogRead(lmanalogpin);
   }
-  tempInC =  (referenceVoltage * reading * 10) / 1023;
+  tempInC =  ((referenceVoltage * reading * 10) / 1023 + SENSOR_OFFSET);
   return tempInC;
 }
 
@@ -92,9 +104,17 @@ void setupSensors() {
   if (!sensors.getAddress(sensor1, 0))
   {
     lcd.setCursor(0, 0);
-    lcd.print("DS SENSOR");
+    lcd.print("VERIFY");
     lcd.setCursor(0, 1);
-    lcd.print("NOT FOUND!");
+    lcd.print("AMBIENT SENSOR!");
+    delay(10000);
+  }
+  float tempReading = readLm35Temp(LM_35_APIN);
+  if(tempReading > 70 || tempReading < 0){
+    lcd.setCursor(0, 0);
+    lcd.print("VERIFY");
+    lcd.setCursor(0, 1);
+    lcd.print("WATER SENSOR!");
     delay(10000);
   }
   //disabled the wait for conversion on the ds18. It normally adds a 1sec delay
